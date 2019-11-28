b7ebf167ff21941dbce0a3c19f32f72fab24f298
Nathakorn Throngtrairat <nathakorn.throngtrairat@agoda.com>
2019-11-28 22:20:47 +0700

    Add CORS

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,13 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(bodyParser.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials', "true")
+    next()
+})
 
 register(app, HomeRouterController)
 
